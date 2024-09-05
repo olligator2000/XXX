84f3178f3fdf944016eb1666586ca560815d5f7e
olligator2000 <61042566+olligator2000@users.noreply.github.com>
2024-09-05 20:00:37 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -225,20 +225,398 @@
 //     alert(sum); 
 
 // Пользователь вводит два числа ( первое < второго). Вывести сумму всех чётных чисел между ними.
-var x = parseInt(prompt("Введите первое число"));
-var y = parseInt(prompt("Введите второе число"));
-var sum = 0;
+// var x = parseInt(prompt("Введите первое число"));
+// var y = parseInt(prompt("Введите второе число"));
+// var sum = 0;
 
-for(i = x; i <= y ; i++) {
-       if (i % 2 == 0) {
-        sum = sum + i;
-       } 
-    }
-    alert(sum); 
+// for(i = x; i <= y ; i++) {
+//        if (i % 2 == 0) {
+//         sum = sum + i;
+//        } 
+//     }
+//     alert(sum); 
+//---------------------------------------------------
+// var answer;
+// line = [];
+// for (i = 2; i < 10; i++) {
+//         for (j = 1; j < 11; j++) {
+//                 answer = i * j;
+//                 line.push(answer);
+               
+//         }
+//         document.write(line);
+//         document.write('<br>');
+//         line=[];
+// }
+//---------------------------------------------------
+
+// let a = [465, 564, 98797, 13, 324, 56742, 648, 243333374357,5476566,56465];
+// let max = 0;
+// for(i = 0; i < a.length; i++) {
+//         if (a[i] > max) {
+//                 max = a[i];
+//         }
+// }alert(max);
+//---------------------------------------------------
+// let a = [465, 564, 98797, 13, 324, 56742, 648, 243333374357,5476566,56465];
+// for(i = 0; i < a.length; i++) {
+//     for(k = i; k < i; k++) {
+//         if(a[i] > a[k]) {
+//             a[i] = a[k];
+//         }
+//     }
+// }
+// alert[a];
+//---------------------------------------------------
+
+// if (confirm("загадай целое число от 0 до 100")) {
+//     var arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+//     var N;
+//     for(i = 0; i <= 100; i++) {
+//         N  =  arr[Math.floor(arr.length / 2)];
+//         var symbol = prompt("Ваше число > " + N + " или < " + N + " или = " + N + " ?");
+//         if(symbol == ">") {
+//             for(j = 1; j < arr.length; j++) {
+//                 arr[j-1] = arr[Math.floor(arr.length / 2)+j];
+//                 arr[j] = arr[j-1];
+//             }
+//             arr.length = Math.floor(arr.length / 2);
+//         }  else if(symbol == "<") {
+//             arr.length = Math.floor(arr.length / 2);
+//         } else {
+//             alert("Я угодал!!!=)")
+//             break;
+//         }
+//         if(arr.length == 1) {
+//             N = arr;
+//             var symbol = prompt("Ваше число = " + N + " ?");
+//             if(symbol == "=") {
+//                 alert("Я угодал!!!=)")
+//                 break;
+//             }
+//         }
+//     } 
+// }
+//----------------------------------------------------
+// var number = prompt("Введите число").split("");
+// var step = parseInt(prompt("Введите на сколько сдвинуть число число"));
+// for (var i = 0; i < step; i++) {
+//   var arr = number[0];
+//   for (var j = 1; j < number.length; j++) {
+//     number[j - 1] = number[j];
+//     number[j] = number[j + 1];
+//   }
+//   number[number.length - 1] = arr;
+// }
+// alert(number.join(""));
+
+//----------------------------------------------------
+// var x = parseInt(prompt("Введите любое число"));
+// function minus(x) {
+// if (x < 0) {
+//     return -1
+// } return 1
+// }
+
+// alert(minus(x));
+//----------------------------------------------------
+// function bolmen(a, b) {
+// if (a < b) {
+//     return a + "<" + b;
+// } else if (a > b) {
+//     return a + ">" + b;
+// } else {
+//     return a + "=" + b;
+// }
+// }
+// let number = bolmen(+prompt("Введите первое число"), +prompt("Введите второе число"))
+// alert(number);
+//----------------------------------------------------
+// function stepen(a, s) {
+//     sum = 1;
+//     for (let i = 1; i <= s; i++) {
+//         sum *= a;
+//     }
+//     return sum;
+// }
+// let number = stepen(+prompt("Введите целое число"), +prompt("Введите в какую степень возвести"))
+// alert(number);
+//----------------------------------------------------
+// function calc(x, symbol, y) {
+//     let answer;
+//     if(symbol == "/") {
+//         if(y == 0) {
+//             return "На ноль делить нельзя"
+//         }
+//         answer =  x / y
+//     } else if(symbol == "*") {
+//     answer =  x * y
+//     } else if(symbol == "-") {
+//     answer =  x - y
+//     } else if(symbol == "+") {
+//     answer =  x + y
+//     }
+//     return answer;
+// }
+// alert(calc((parseInt(prompt("Введите первое число"))), prompt("Введите операцию '/' или '*' или '-' или '+'"), parseInt(prompt("Введите второе число"))));
+//----------------------------------------------------
+// function summa(a, b, c, d, e) {
+//     if (isNaN(a)) {
+//         a = 0;
+//     } 
+//     if(isNaN(b)) {
+//         b = 0;
+//     }
+//     if(isNaN(c)) {
+//         c = 0;
+//     } 
+//     if(isNaN(d)) {
+//         d = 0;
+//     }
+//     if(isNaN(e)) {
+//         e = 0;
+//     } 
+//     let answer = a + b + c + d + e;
+//     return answer;
+// }
+// alert(summa((parseInt(prompt("Введите первое число"))), (parseInt(prompt("Введите второе число"))), (parseInt(prompt("Введите третье число"))), (parseInt(prompt("Введите четвертое число"))), (parseInt(prompt("Введите пятое число")))));
+
+//----------------------------------------------------
+// function sum() {
+//     arr = 0;
+//     for(i = 0; i < arguments.length; i++) {
+//         arr += arguments[i];
+//     }
+// return arr;
+// }
+// alert(sum(5,5));
+//----------------------------------------------------
+// function min() {
+//         minimum = arguments[0];
+//         for(i = 0; i < arguments.length; i++) {
+//             if(arguments[i] < minimum) {
+//                 minimum = arguments[i];
+//             }
+//         }
+//     return minimum;
+//     }
+//     alert(min(5,5,-5,-100,69,0,1));
+//----------------------------------------------------
+// function stroka() {
+//     s = '';
+//     for(i = 0; i < arguments.length; i++) {
+//         s += arguments[i] + ' ';
+//     }
+//     return s;
+// }
+// alert(stroka("Привет", "Олег", "как", "дела","?"))
+//----------------------------------------------------
+// function massivData() {
+//     mData  = [];
+//     for(i = 0; i < arguments.length; i++) {
+//         if(typeof arguments[i] == "number") {
+//             mData.push(arguments[i]);
+//         }
+//     }
+//     return mData;
+// }
+// alert(massivData(NaN, undefined, false, true, 5, 3, Infinity));
+// alert(typeof -Infinity);
+//----------------------------------------------------
+// function stroka(a, b) {
+//     if (a.length > b.length) {
+//         return 1;
+//     } else if(a.length < b.length) {
+//         return -1;
+//     } else {
+//         return "Длина строк одинакова";
+//     }
+// }
+// alert(stroka(prompt("введите первую строку"), prompt("введите вторую строку")));
+//----------------------------------------------------
+
+// function stroka(a) {
+//     return a[0].toUpperCase() + a.slice(1);
+// }
+// alert(stroka(prompt("введите cтроку")));
+//----------------------------------------------------
+// function stroka(str) {
+//     g = "уеаоэяиюы"; 
+//     let counter = 0;
+//     for(let i = 0; i < str.length; i++) {
+//         if(g.includes(str[i])) {
+//             counter += i;
+//         }
+//     }
+// }
+// alert(stroka(prompt("Введите строку")));
+//----------------------------------------------------
+// function chek_spam(str) {
+//     const spam_words = ['100 безплатно завтра','увелечение продаж', 'только сегодня', 'не удаляйте'];
+
+//     let lower_case_str = str.toLowerCase();
+//     for(let i = 0; i < lower_case_str; i++) {
+//         if (lower_case_str.includes(spam_words[i])) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }   
+// console.log(chek_spam('100 безплатно завтра'));
+// console.log(chek_spam('завтра'));
+// console.log(chek_spam('только сегодня'));
+//----------------------------------------------------
+// const student = {
+//     name: 'Oleg',
+//     surname: 'Petrov',
+//     age: 20,
+//     is_student: true,
+// }
+// console.log(student);
+// console.log(student.name);
+// delete student.age;
+// console.log(student);
+
+// for (let j in student) {
+//     console.log(j);
+//     console.log(student[j]);
+// }
+//----------------------------------------------------
+// const car = {
+//     make: 'AutoVAZ',
+//     model: 'Lada Vesta',
+//     year: 2018,
+// }
+// // console.log(car.make + car.model + car.year);
+// console.log(`Make: ${car.make}, Model: ${car.model}, Make: ${car.year}`);
+//----------------------------------------------------
+// const person = {
+//     name: "Oleg",
+//     age: 40,
+// }
+// person.gender = "Man";
+// person.age = 21;
+// console.log(person);
+//----------------------------------------------------
+// const book = {
+//     title: "Мастер и Маргарита",
+//     author: "Михаил Булгаков",
+//     year: 1940,
+// }
+// delete book.year;
+// console.log(book);
+//----------------------------------------------------
+// const user = {
+//     username: "Terminator",
+//     email: "arni@gmail.com",
+//     password: 4242,
+// }
+// for(let i in user) {
+//     console.log(i + " - " + user[i]);
+// }
+//----------------------------------------------------
+// const company = {
+//     address: {
+//         street: "Lenina",
+//         city: "Ryazan",
+//         zipCode: 234123,
+//     }
+// }
+// console.log(`Street: ${company.address.street}, City: ${company.address.city}, Zip Code: ${company.address.zipCode},`);
+//----------------------------------------------------
+// const students = [
+//     {
+//         name: "Petrov",
+//         bal: 5,
+//     },
+//     {
+//         name: "Ivanov",
+//         bal: 5,
+//     },
+//     {
+//         name: "Frolov",
+//         bal: 5,
+//     },
+//     {
+//         name: "Sidorov",
+//         bal: 5,
+//     },
+//     {
+//         name: "Masunov",
+//         bal: 5,
+//     },
+//     {
+//         name: "Hrenov",
+//         bal: 5,
+//     },
+// ]
+// function sred_bal(a) {
+//     let summ = 0;
+//     let summ_sr = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         summ += a[i].bal;
+//     }
+//     summ_sr = summ / a.length;
+//     return summ_sr;
+// }
+// console.log(sred_bal(students));
+//----------------------------------------------------
+// const settings = {
+//     volume: 5,
+//     date: 24,
+//     hren: 2,
+// }
+// function blablabla(a, b) {
+//     for(let key in a) {
+//         if (typeof a[key] === 'number') {
+//             a[key] += b
+//         }
+//     }
+//     return a;
+// }
+// console.log(blablabla(settings, 1));
+//----------------------------------------------------
+// const car_settings = {
+//     make: "LADA",
+//     model: "Vesta",
+//     age: 2018,
+//     speed: 70,
+// }
+
+// function info_car(a) {
+//     console.log(`Make: ${car_settings.make}, Model: ${car_settings.model}, Age: ${car_settings.age}, Speed: ${car_settings.speed}`); 
+// }
+
+// info_car(car_settings)
+
+// function time(obj, s) {
+//         let time = s / obj.speed;
+//         let delay = Math.floor(time / 4);
+//         let total_time = time + delay;
+
+//         return total_time;
+// }
+// console.log(time(car_settings, 1050));
 
 
 
 
+//--------------------- OOP--------------------------------------------------------
 
 
 
+class Human {
+        constructor(name, surname, age) {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+        }
+        display_info() {
+            console.log(`Name: ${this.name}, Surname: ${this.surname}, Age: ${this.age}`);
+        }
+}
+const oleg = new Human('Oleg', 'Ivanov', '40');
+const alex = new Human('Alex', 'Petrov', '35');
+
+oleg.display_info();
+alex.display_info();
