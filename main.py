c9ffacb259202388a80be6a60a5c24142fc5bb1d
olligator2000 <61042566+olligator2000@users.noreply.github.com>
2024-09-17 21:19:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# name = 'Alex'  # str()
+# age = 15  # int()
+# height = 190  # int()
+# is_student = True  # bool()
+
+# print(type(name), type(age), type(is_student))  # Выводим тип данных
+# print(type(str(age)))  # Переводим из цифры в стоку
+# print(12 // 5)  #  Отбрасывает всё после запятой
+
+# age = int(input('Введите ваше число: '))
+# print(age)
+###############################
+# print('Привет\nОлег\nВладимирович')
+# print('Как\n\tдела\n\t\tбатька\n\t\t\t\t\t\t\tМахно?')
+###############################
+# user_data1 = int(input('Введите первое число: '))
+# user_data2 = int(input('Введите второе число: '))
+# print(user_data1 + user_data2)
+# print(user_data1 - user_data2)
+# print(user_data1 * user_data2)
+# print(user_data1 / user_data2)
+################################
+# user_data1 = int(input('Введите первое число: '))
+# user_data2 = int(input('Введите процент: '))
+# proc = user_data1 / 100 * user_data2
+# print(user_data2, 'процентов от ', user_data1, ' = ', proc)
+################################
+# user_data1 = int(input('Введите длину первой стороны: '))
+# user_data2 = int(input('Введите длину второй стороны: '))
+# s = user_data1 * user_data2
+# print(s)
+################################
+# user_data1 = int(input('Введите первую цифру: '))
+# user_data2 = int(input('Введите вторую цифру: '))
+# print(str(user_data1) + str(user_data2))
+################################
+# user_data1 = int(input('Введите температуру: '))
+# f = (9 / 5) * user_data1 + 32
+# print(f)
+
+
+
+
+
+
+
