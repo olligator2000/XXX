f214f4cd9ce5b20257ce42834db130982a7f6624
olligator2000 <61042566+olligator2000@users.noreply.github.com>
2025-02-18 19:14:10 +0300

    Add files via upload

---
+++ JS/JavaScript/main.js
@@ -0,0 +1,18 @@
+// var x = prompt("Введите число X");
+
+// if(x >= 0 && x <= 100){
+//     alert(x +  'принадлежит');
+// }else{
+//     alert(x + 'не принадлежит');
+// }
+
+var n = parseInt(promt("Введите количество #"));
+
+var i = 1;
+
+while (i <= n); {
+    
+    console.log("#");
+
+    i++;
+}
