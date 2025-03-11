24f2782cb4a62e352f347b8a9c993e8d873990e6
olligator2000 <61042566+olligator2000@users.noreply.github.com>
2025-03-11 21:41:30 +0300

    Add files via upload

--- Python/TELEGRAM_BOT.py
+++ Python/TELEGRAM_BOT.py
@@ -183,67 +183,240 @@
 
 ####################################################### Бот для поиска фильмов по названию и году выпуска
 
+# import telebot
+# import requests
+# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+#
+# BOT_API = "7683989927:AAExSrCaEwgYo_90HFYUVIgn5DIWmJTarjk"
+# OMDB_API_KEY = "caf5b564"
+#
+# bot = telebot.TeleBot(BOT_API)
+#
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     markup = InlineKeyboardMarkup()
+#     button_title = InlineKeyboardButton("Поиск по названию фильма", callback_data='search_by_title')
+#     button_year = InlineKeyboardButton("Поиск по году выпуска", callback_data='search_by_year')
+#     markup.add(button_title, button_year)
+#
+#     bot.send_message(message.chat.id, "😎Привет! Я бот для поиска фильмов.😎\n"
+#                                         "Выбери способ поиска:", reply_markup=markup)
+#
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_query(call):
+#     if call.data == 'search_by_title':
+#         bot.send_message(call.message.chat.id, "Напиши название фильма на английском:")
+#         bot.answer_callback_query(call.id)
+#     elif call.data == 'search_by_year':
+#         bot.send_message(call.message.chat.id, "Напиши год выпуска фильма:")
+#         bot.answer_callback_query(call.id)
+#
+# def get_movie_info(title):
+#     url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
+#     response = requests.get(url)
+#     data = response.json()
+#
+#     if data.get("Response") == "True":
+#         movie_info = (
+#             f"🎥 *{data['Title']}* *({data['Year']})*\n"
+#             f"🌟 *Рейтинг:* {data['imdbRating']}\n"
+#             f"🎬 *Жанр:* {data['Genre']}\n"
+#             f"👤 *Режиссер:* {data['Director']}\n"
+#             f"👽 *Актеры:* {data['Actors']}\n"
+#             f"💬 *Описание:* {data['Plot']}\n"
+#         )
+#         poster = data.get("Poster", "")
+#         return movie_info, poster
+#     else:
+#         return "Фильм по такому запросу не найден!", None
+#
+# @bot.message_handler(func=lambda message: True)
+# def search_movie(message):
+#     # Попробуем сначала извлечь год из сообщения
+#     try:
+#         year = int(message.text)
+#         # Здесь я добавлю логику для поиска по году. Например, вы можете выполнять запросы к API, чтобы вернуть фильмы за этот год
+#         bot.send_message(message.chat.id, f"Ищу фильмы, выпущенные в {year}...")
+#         # Здесь можно вызвать функцию поиска фильмов по году
+#     except ValueError:
+#         # Если это не год, то искать по названию фильма
+#         movie_info, poster = get_movie_info(message.text)
+#         if poster and poster != "N/A":
+#             bot.send_photo(message.chat.id, poster, caption=movie_info, parse_mode="Markdown")
+#         else:
+#             bot.send_message(message.chat.id, movie_info, parse_mode="Markdown")
+#
+# bot.polling()
+
+
+####################################################### Бот прогноза погоды
+
+
+# import telebot
+# import requests
+# from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+#
+#
+# BOT_API = "7770590397:AAHBSzmBrZLiZxYe5JJHTh-2QvTm2K1I3r8"
+# WEATHER_API_KEY = "b73eeacce06afd66ec194738c9a4e57d"
+#
+# bot = telebot.TeleBot(BOT_API)
+#
+#
+# def main_menu():
+#     markup = ReplyKeyboardMarkup(row_width=2)
+#     button_1 = KeyboardButton("Москва")
+#     button_2 = KeyboardButton("Санкт-Петербург")
+#     button_3 = KeyboardButton("Ввести город")
+#     markup.add(button_1, button_2, button_3)
+#     return markup
+#
+#
+# def get_weather(city):
+#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={WEATHER_API_KEY}'
+#     response = requests.get(url).json()
+#
+#     if response.get("main"):
+#         return (
+#             f'Город: {city}\n'
+#             f'Температура: {response["main"]["temp"]}°C\n'
+#             f'Влажность: {response["main"]["humidity"]}%\n'
+#             f'Ветер: {response["wind"]["speed"]}м/с\n'
+#             f'Давление: {response["main"]["pressure"]}рт.ст.\n'
+#         )
+#     else:
+#         return "Город не найден"
+#
+#
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.send_message(message.chat.id, "😎Привет! Я бот для прогноза погоды по городу.😎\n"
+#                                         "Выбери город или введи название:", reply_markup=main_menu())
+#
+#
+# @bot.message_handler(func=lambda message: message.text in ["Москва", "Санкт-Петербург"])
+# def handle_buttons(message):
+#     city = message.text
+#     bot.send_message(message.chat.id, get_weather(city))
+#
+#
+# @bot.message_handler(func=lambda message: message.text == "Ввести город")
+# def handle_buttons(message):
+#     bot.send_message(message.chat.id, "Введите название города")
+#     bot.register_next_step_handler(message, send_weather)
+#
+#
+# def send_weather(message):
+#     city = message.text
+#     bot.send_message(message.chat.id, get_weather(city))
+#
+#
+# bot.polling()
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+# 3. Бот-анализатор новостей
+# Описание:
+# Бот ищет актуальные новости по ключевым словам и отправляет пользователю.
+# Функционал:
+# ✅ Получение свежих новостей (API: NewsAPI)
+# ✅ Подписка на темы (технологии, спорт, экономика)
+# ✅ Автоматическая отправка новостей каждый день
+# ✅ Анализ тональности (позитивная/негативная)
+# ✅ Перевод заголовков на русский (Google Translate API)
+# Как сделать круче?
+# • Добавить автоматическое резюме новостей с помощью GPT API.
+# • Сделать голосовое озвучивание новостей (pyttsx3).
+# • Подключить возможность фильтровать фейковые новости.
+
 import telebot
 import requests
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from newsdataapi import NewsDataApiClient
 
-BOT_API = "7683989927:AAExSrCaEwgYo_90HFYUVIgn5DIWmJTarjk"
-OMDB_API_KEY = "caf5b564"
+BOT_API = "7770590397:AAHBSzmBrZLiZxYe5JJHTh-2QvTm2K1I3r8"
+NEWS_API_KEY = "pub_740976a50713043bac6ec9aa65409f1ec42e6"
 
 bot = telebot.TeleBot(BOT_API)
 
+
+def main_menu():
+    markup = ReplyKeyboardMarkup(row_width=2)
+    button_1 = KeyboardButton("Получить последние новости")
+    button_2 = KeyboardButton("Подписаться/отписаться на темы")
+    button_3 = KeyboardButton("Получить последние новости по подписке")
+    button_4 = KeyboardButton("Вкл/выкл автоматическую отправку новостей по подписке каждый день")
+    button_5 = KeyboardButton("Информация/Помощь")
+    markup.add(button_1, button_2, button_3, button_4, button_5)
+    return markup
+
+
+def main_menu_subscription():
+    markup = ReplyKeyboardMarkup(row_width=2)
+    button_1 = KeyboardButton("Наука")
+    button_2 = KeyboardButton("Музыка")
+    button_3 = KeyboardButton("Технологии")
+    button_4 = KeyboardButton("Спорт")
+    button_5 = KeyboardButton("Экономика")
+    button_6 = KeyboardButton("Назад ↩️")
+    markup.add(button_1, button_2, button_3, button_4, button_5, button_6)
+    return markup
+
+
+def latest_news(news):
+    url = f'https://newsdata.io/api/1/latest?apikey={NEWS_API_KEY}&country=ru&language=ru'
+    response = requests.get(url).json()
+
+    if response.get("main"):
+        return (
+            f'Название статьи: {title}\n'
+            f'Температура: {response["main"]["temp"]}°C\n'
+            f'Влажность: {response["main"]["humidity"]}%\n'
+            f'Ветер: {response["wind"]["speed"]}м/с\n'
+            f'Давление: {response["main"]["pressure"]}рт.ст.\n'
+        )
+    else:
+        return "Город не найден"
+
+# def get_weather(city):
+#     url = f'https://newsdata.io/api/1/latest?apikey={NEWS_API_KEY}&q={}'
+#     response = requests.get(url).json()
+#
+#     if response.get("main"):
+#         return (
+#             f'Название статьи: {title}\n'
+#             f'Температура: {response["main"]["temp"]}°C\n'
+#             f'Влажность: {response["main"]["humidity"]}%\n'
+#             f'Ветер: {response["wind"]["speed"]}м/с\n'
+#             f'Давление: {response["main"]["pressure"]}рт.ст.\n'
+#         )
+#     else:
+#         return "Город не найден"
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = InlineKeyboardMarkup()
-    button_title = InlineKeyboardButton("Поиск по названию фильма", callback_data='search_by_title')
-    button_year = InlineKeyboardButton("Поиск по году выпуска", callback_data='search_by_year')
-    markup.add(button_title, button_year)
+    bot.send_message(message.chat.id, "😎Привет! Я бот новостей.😎\n"
+                                        "Выбери город или введи название:", reply_markup=main_menu())
 
-    bot.send_message(message.chat.id, "😎Привет! Я бот для поиска фильмов.😎\n"
-                                        "Выбери способ поиска:", reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    if call.data == 'search_by_title':
-        bot.send_message(call.message.chat.id, "Напиши название фильма на английском:")
-        bot.answer_callback_query(call.id)
-    elif call.data == 'search_by_year':
-        bot.send_message(call.message.chat.id, "Напиши год выпуска фильма:")
-        bot.answer_callback_query(call.id)
+@bot.message_handler(func=lambda message: message.text in ["Москва", "Санкт-Петербург"])
+def handle_buttons(message):
+    city = message.text
+    bot.send_message(message.chat.id, get_weather(city))
 
-def get_movie_info(title):
-    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
-    response = requests.get(url)
-    data = response.json()
 
-    if data.get("Response") == "True":
-        movie_info = (
-            f"🎥 *{data['Title']}* *({data['Year']})*\n"
-            f"🌟 *Рейтинг:* {data['imdbRating']}\n"
-            f"🎬 *Жанр:* {data['Genre']}\n"
-            f"👤 *Режиссер:* {data['Director']}\n"
-            f"👽 *Актеры:* {data['Actors']}\n"
-            f"💬 *Описание:* {data['Plot']}\n"
-        )
-        poster = data.get("Poster", "")
-        return movie_info, poster
-    else:
-        return "Фильм по такому запросу не найден!", None
+@bot.message_handler(func=lambda message: message.text == "Ввести город")
+def handle_buttons(message):
+    bot.send_message(message.chat.id, "Введите название города")
+    bot.register_next_step_handler(message, send_weather)
 
-@bot.message_handler(func=lambda message: True)
-def search_movie(message):
-    # Попробуем сначала извлечь год из сообщения
-    try:
-        year = int(message.text)
-        # Здесь я добавлю логику для поиска по году. Например, вы можете выполнять запросы к API, чтобы вернуть фильмы за этот год
-        bot.send_message(message.chat.id, f"Ищу фильмы, выпущенные в {year}...")
-        # Здесь можно вызвать функцию поиска фильмов по году
-    except ValueError:
-        # Если это не год, то искать по названию фильма
-        movie_info, poster = get_movie_info(message.text)
-        if poster and poster != "N/A":
-            bot.send_photo(message.chat.id, poster, caption=movie_info, parse_mode="Markdown")
-        else:
-            bot.send_message(message.chat.id, movie_info, parse_mode="Markdown")
+
+def send_weather(message):
+    city = message.text
+    bot.send_message(message.chat.id, get_weather(city))
+
 
 bot.polling()
